8aae7f43f2cbfac75e44fd4b5f560a2cd6d68acb
Anwesh43 <anweshthecool0@gmail.com>
2021-04-17 02:46:50 +0530

    adding constants and libraries

--- linebarcreatorview/src/main/java/com/example/linebarcreatorview/LineBarCreatorView.kt
+++ linebarcreatorview/src/main/java/com/example/linebarcreatorview/LineBarCreatorView.kt
@@ -1,2 +1,26 @@
 package com.example.linebarcreatorview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#009688",
+    "#FF5722",
+    "#1A237E",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 6.9f
